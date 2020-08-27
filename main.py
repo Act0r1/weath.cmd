77e3af2fdca876631805c2ee32121ac63adbc7d2
Insaf <59477383+OneZeroOne101@users.noreply.github.com>
2020-08-27 18:23:35 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+payload = {'m':'','T':'','q':'','u':'','lang':'ru'}
+cities = ['Лондон', 'svo', 'Череповец']
+for city in cities:
+    url = f'http://wttr.in/{city}'
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    print(response.text)
+#http://wttr.in/london?mTqu&lang=ru
